ebc4011959b7a7a586865b4eb393907565bcd713
Aparajita <aparajita@aparajita.com>
2024-04-21 16:53:50 -0700

    feat!: upgrade to Capacitor 6, Ionic 8, plugin 6

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-/* Core CSS required for Ionic components to work properly */
+// Core CSS required for Ionic components to work properly
 import '@ionic/vue/css/core.css'
 
-/* Basic CSS for apps built with Ionic */
+// Basic CSS for apps built with Ionic
 import '@ionic/vue/css/normalize.css'
 import '@ionic/vue/css/structure.css'
-import '@ionic/vue/css/typography.css'
+import '@ionic/vue/css/padding.css'
 
-/* Theme variables */
-import './theme/variables.css'
+// Dark mode support
+import '@ionic/vue/css/palettes/dark.system.css'
+
+/* Custom CSS */
 import './assets/css/styles.pcss'
 
 const config: Record<string, unknown> = {
